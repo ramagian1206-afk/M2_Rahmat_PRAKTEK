735e324270855b0f0caa393e8f07122721c5e965
Arini <balqisarinibalqsi@gmail.com>
2026-03-13 13:11:04 +0700

    PRAKTEK_2_Rahmat

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Weapon pedangSakti = new Weapon();
+
+        pedangSakti.name = "Malefic";
+        pedangSakti.damage = 150;
+
+        pedangSakti.showWeapon();
+    }
+}
